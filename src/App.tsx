ffc6b14bfb6dfb01341c88321792e896f9bd8860
duwan <duwandc969@gmail.com>
2024-10-24 01:46:11 -0800

    'fix/btnnamegrid'

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthPage } from "./Pages/Auth";
-import { MainPage } from "./Pages/MainPage";
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/auth" Component={AuthPage} />
-        <Route index path="/main-page" Component={MainPage} />
-        <Route path="*" element={<Navigate to="/auth" />} />
-      </Routes>
-    </BrowserRouter>
+
+    <div className="size-full bg-white flex justify-center items-center">
+      <GameBoard />
+    </div>
   );
 }
 
